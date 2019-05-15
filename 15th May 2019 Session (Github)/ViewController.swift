071106c8ce89ec4323c04f98c01c2e9bc01ee312
Clieff <pukuflu@gmail.com>
2019-05-15 08:32:35 +0700

    Initial Commit

---
+++ 15th May 2019 Session (Github)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  15th May 2019 Session (Github)
+//
+//  Created by Clieff Tyhonksky on 15/05/19.
+//  Copyright © 2019 Clieff Tyhonksky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
